32df74a55bca308be96a39666435c84be9ba418a
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-04-14 20:28:16 +0900

    5回目のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,15 @@
 $('.voice__content--list').slick({  
-    autoplay: true, //自動再生
+        autoplay: true, //自動再生
         speed: 5000,//スライドするスピード
-        arrows: true, // 左右のナビゲーションを有効にする
+        arrows: false, // 左右のナビゲーションを有効にする
         dots: true, // ドットを表示
         infinite: true, //スライドのループ
         slidesToShow: 3,
         slidesToScroll: 3,
         adaptiveHeight: true,
-
+        variableWidth: true,
+        
+        
         responsive: [{
             breakpoint: 768,
             settings: {
