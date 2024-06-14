6e06f6c1b7d283c93eff7a1587ee7cd8d695a049
CWWnckw25 <yitchikuwaling512@gmail.com>
2024-06-14 17:42:04 +0900

    修正後

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,22 +1,21 @@
-$('.voice__content--list').slick({  
-        autoplay: true, //自動再生
-        speed: 5000,//スライドするスピード
-        arrows: false, // 左右のナビゲーションを有効にする
-        dots: true, // ドットを表示
-        infinite: true, //スライドのループ
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        adaptiveHeight: true,
-        variableWidth: false,
-        
-        
-        responsive: [{
-            breakpoint: 768,
-            settings: {
+$('.voice__content--list').slick({
+    autoplay: true,
+    speed: 5000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+
+    responsive: [{
+        breakpoint: 768,
+        settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
             centerMode: true,
             centerPadding: "10%",
-            }
-        }]
-    });
+            variableWidth: true,
+        }
+    }]
+});
